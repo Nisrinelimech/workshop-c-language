ce37505dda750bf6bae95c01477a441fa18b1af5
unknown <nisrine.elimech349@gmail.com>
2022-09-09 10:49:45 +0100

    challenge.9

--- week_01/challenge9.c
+++ week_01/challenge9.c
@@ -12,6 +12,6 @@ int main()
    scanf("%f,%f",& x1,& y1) ;
    printf("entrez les coordonnees du deuxieme point (x2, y2) :") ;
    scanf("%f,%f",& x2,& y2) ;
-   distance = sqrt( pow( x2 - x1, 2) + pow( y2 - y1, 2)) ;
+   distance = sqrt( pow( (x2 - x1), 2) + pow( (y2 - y1), 2)) ;
    printf("la distance entre les deux points de coordonnees (%f,%f) et (%f,%f) est %f:", x1, y1, x2, y2, distance) ;
 }
