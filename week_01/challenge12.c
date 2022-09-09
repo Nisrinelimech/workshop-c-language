bd3811c7958d800cd1b5a4d8eb8a085e03d15645
unknown <nisrine.elimech349@gmail.com>
2022-09-09 10:05:58 +0100

    challenge&é

--- week_01/challenge12.c
+++ week_01/challenge12.c
@@ -4,12 +4,9 @@
 int main()
 {
    printf("nombre entier à trois chiffres en ordre inverse\n") ;
-   int nombre ;
-   printf("entrez un nombre entier a trois chiffres:") ;
-   scanf("%d",& nombre) ;
-   int chiffre1, chiffre2, chiffre3 ;
-   chiffre1 = nombre/100 ;
-   chiffre2 = nombre/10 - chiffre1*10 ;
-   chiffre3 = nombre - chiffre1*100 - chiffre2*10;
-   printf("linverse du %d est : %d%d%d", nombre, chiffre3, chiffre2, chiffre1 ) ;
+   printf("entrer un nombre entier a trois chiffres :") ;
+   char nombre[3];
+   scanf("%s", &nombre) ;
+   printf("le nombre en ordre inverse est : %s", strrev(nombre));
+   return 0;
 }
