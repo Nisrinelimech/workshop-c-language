180e923f136f7a1f33b02a5e13d418b72f454af5
unknown <nisrine.elimech349@gmail.com>
2022-09-09 09:28:18 +0100

    challenge11

---
+++ week_01/challenge11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("circonference dun rectangle :\n") ;
+    float langueur ;
+    float largeur ;
+    float circonference ;
+    printf("entrez la langueur du rectangle : ") ;
+    scanf("%f",& langueur);
+    printf("entrez la largeur du rectangle : ") ;
+    scanf("%f",& largeur) ;
+    circonference = 2*(langueur + largeur) ;
+    printf("la ciconference de ce rectangle est : %.2f", circonference) ;
+}
